c51156b2f248da27cdeb4deb76a69906e0855a7c
Aryaman Sriram <76403763+crazycheetah42@users.noreply.github.com>
2022-05-26 17:54:43 -0400

    Update qreader.py

--- qrcodereader/qreader.py
+++ qrcodereader/qreader.py
@@ -1,11 +1,11 @@
 import cv2
 import time
 import sys
+# below variables came from https://www.thepythoncode.com/article/generate-read-qr-code-python
 path = input("Enter the path here: ")
 img = cv2.imread(path)
 read = cv2.QRCodeDetector()
 bbox = read.detectAndDecode(img)
-# below if statement came from https://www.thepythoncode.com/article/generate-read-qr-code-python
 if bbox is not None:
     print(f"QRCode data:\n{bbox}")
     time.sleep(12)
